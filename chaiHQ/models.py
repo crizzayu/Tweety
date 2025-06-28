efd284e84b2fb7038de4e2d4d68dc66caa396102
crizzayu <ayushks9415@gmail.com>
2025-06-29 02:29:25 +0530

    Update models.py

--- chaiHQ/models.py
+++ chaiHQ/models.py
@@ -2,13 +2,14 @@ from django.db import models
 #we are creating a model 
 #import jango usme contrib -> auth -> models milega
 from django.contrib.auth.models import User # this user is from admin panel (editable , configruble)
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 
 class Tweet(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField(max_length=250)
-    photo = models.ImageField(upload_to='photos/',blank=True, null=True)
+    photo = CloudinaryField('image', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
